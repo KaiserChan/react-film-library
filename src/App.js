ddd568729fec3a01ca5cf4220781bb956687e3c3
KaiserChan <chankaiserkk@gmail.com>
2017-10-10 22:45:43 -0400

    updated react film ... completed up to step 5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TMDB from "./TMDB"
+import FilmDetails from "./filmDetails"
+import FilmListings from "./filmListing"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="film-library">
+        <div className="film-list">
+          <h1 className="section-title">Titles</h1>
+          <FilmListings title={TMDB.films.map((item,i) => <li key={i}>item.title</li>)}/>
+        </div>
+
+        <div className="film-details">
+          <h1 className="section-title">DETAILS</h1>
+          <FilmDetails tmdb={TMDB}/>
+        </div>
       </div>
     );
   }
---
+++ src/filmDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FilmDetails extends Component {
+  render() {
+    return (
+      <p> I am film details component </p>
+    )
+  }
+}
+
+export default FilmDetails
